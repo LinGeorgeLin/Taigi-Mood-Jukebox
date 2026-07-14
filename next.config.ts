60cdd86416c404f1f8bf70f72f3a94e8bc4219ec
George <glin7583@gmail.com>
2026-07-14 14:30:46 +0800

    Updates of API URL

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,13 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        // 當前端呼叫 /api/moods 時，Next.js 會在後台默默幫你轉去 Hugging Face
+        // 加上 .direct 才能繞過 Hugging Face 的 HTML 外殼，直接連到 FastAPI 後端
         source: '/api/moods',
-        destination: 'https://georgelin29-taigi-mood-backend.hf.space/api/moods',
+        destination: 'https://georgelin29-taigi-mood-backend.direct.hf.space/api/moods',
       },
       {
-        // 當前端呼叫 /api/mood-asr 時，自動轉去 Hugging Face
         source: '/api/mood-asr',
-        destination: 'https://georgelin29-taigi-mood-backend.hf.space/api/mood-asr',
+        destination: 'https://georgelin29-taigi-mood-backend.direct.hf.space/api/mood-asr',
       },
     ];
   },
